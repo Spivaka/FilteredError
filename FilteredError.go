0cb98c5bf435577d02ebf1605f306debc47b1964
amichayspivak <spivaka85@gmail.com>
2016-12-27 21:12:16 +0200

    filter and tests

---
+++ FilteredError.go
@@ -0,0 +1,41 @@
+package FilteredError
+
+import "strings"
+
+type FilteredError struct {
+	err        error
+	ReplaceMap map[string]string
+}
+
+func NewFilteredError(err error, replacers map[string]string) FilteredError {
+	return FilteredError{ReplaceMap: replacers, err: err}
+}
+
+func (fe FilteredError) AddReplacer(object, replacer string) {
+	if fe.ReplaceMap == nil {
+		fe.ReplaceMap = map[string]string{}
+	}
+	fe.ReplaceMap[object] = replacer
+}
+
+func (fe FilteredError) RemoveReplacer(object string) {
+	if fe.ReplaceMap == nil {
+		return
+	}
+	delete(fe.ReplaceMap, object)
+}
+
+func (fe FilteredError) Error() (returnMessage string) {
+	if fe.err == nil {
+		return ""
+	}
+	returnMessage = fe.err.Error()
+	if fe.ReplaceMap == nil {
+		return
+	}
+	for k, v := range fe.ReplaceMap {
+		returnMessage = strings.Replace(returnMessage, k, v, -1)
+	}
+
+	return
+}
---
+++ FilteredError_test.go
@@ -0,0 +1,43 @@
+package FilteredError
+
+import (
+	"testing"
+	"errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCtr(t *testing.T) {
+	err := errors.New("moshe")
+	replaceMap := map[string]string{"yaakov":"yossi"}
+	newErr := NewFilteredError(err, replaceMap)
+	assert.EqualValues(t, err, newErr.err)
+	assert.EqualValues(t, replaceMap, newErr.ReplaceMap)
+}
+
+func TestAddRemoveKeys(t *testing.T) {
+	err := errors.New("moshe")
+	replaceMap := map[string]string{"yaakov":"yossi"}
+	newErr := NewFilteredError(err, replaceMap)
+	assert.EqualValues(t, err, newErr.err)
+	assert.EqualValues(t, replaceMap, newErr.ReplaceMap)
+
+	newErr.RemoveReplacer("yaakov")
+	assert.Len(t, newErr.ReplaceMap, 0)
+
+	newErr.AddReplacer("lili", "lala")
+	assert.Len(t, newErr.ReplaceMap, 1)
+	assert.EqualValues(t, "lala", newErr.ReplaceMap["lili"])
+}
+
+func TestReplace(t *testing.T) {
+	err := errors.New("moshe akiva")
+	replaceMap := map[string]string{"moshe":"yossi"}
+	newErr := NewFilteredError(err, replaceMap)
+	assert.EqualValues(t, err, newErr.err)
+	assert.EqualValues(t, replaceMap, newErr.ReplaceMap)
+
+	assert.EqualValues(t, "yossi akiva", newErr.Error())
+
+	newErr.AddReplacer("akiva", "levin")
+	assert.EqualValues(t, "yossi levin", newErr.Error())
+}
